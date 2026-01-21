e14ff86267e5ff77bd16523641a36f324615a73c
Muhammad Amjad <amjad.venv@gmail.com>
2026-01-21 18:06:21 +0000

    Solved First Question

---
+++ Kotlin_Questions/What.kt
@@ -0,0 +1,8 @@
+// What is kotlin and why it is prefferred over java?
+
+fun main(){
+    println("Kotlin is a modern and statically typed programming language. It is mainly used for Android Development.")
+    println("Why it is preferred over java?")
+    println("Less code, Build-in-null safety, modern features like data classes lambda coroutines 100% java protability and official Android langauge.")
+}
+
